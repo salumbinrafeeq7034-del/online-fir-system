31a9767c260feab3d62ed1d985fc3a9c7fec6899
Salum Bin Rafeeq <salumbinrafeeq7034@gmail.com>
2026-04-14 14:39:52 +0530

    Update DatabaseConnection.java

--- src/main/java/com/fir/onlinesystem/DatabaseConnection.java
+++ src/main/java/com/fir/onlinesystem/DatabaseConnection.java
@@ -1,11 +1,1 @@
-package com.fir.onlinesystem;
-import java.sql.*;
-public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/fir_system"; // Update DB name if different
-    private static final String DB_USER = "root";       // Change to your MySQL username
-    private static final String DB_PASSWORD = "S@lu7034"; // Change to your MySQL password
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, DB_USER, DB_PASSWORD);
-    }
-}
