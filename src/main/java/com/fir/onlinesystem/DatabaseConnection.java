3ca3c986c81e3b86aacaa0c998f6c22825c83987
Salum Bin Rafeeq <salumbinrafeeq7034@gmail.com>
2026-04-14 15:18:13 +0530

    Update DatabaseConnection.java

--- src/main/java/com/fir/onlinesystem/DatabaseConnection.java
+++ src/main/java/com/fir/onlinesystem/DatabaseConnection.java
@@ -1,1 +1,20 @@
+package com.fir.onlinesystem; 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+public class DatabaseConnection {
+    public static Connection getConnection() {
+        try {
+            String dbUrl = System.getenv("DB_URL");
+            String dbUser = System.getenv("DB_USER");
+            String dbPassword = System.getenv("DB_PASSWORD");
+            
+            return DriverManager.getConnection(dbUrl, dbUser, dbPassword);
+        }
+        catch (SQLException e) {
+            System.out.println("Database connection failed!");
+            return null;
+        }
+    }
+}
